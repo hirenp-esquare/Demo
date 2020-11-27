35468ac1bace3a1b23ef90298ad7f24b1ef7954c
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2020-11-27 18:03:06 +0530

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,1 +1,6 @@
-echo $1
+for changed_file in testModelData/agg_v15_2019_06_14.mdl testModelData/cc_v36_2019_09_06.mdl testModelData/mm_v36_2019_09_06.txt; do
+  if [[ ${changed_file} = **.mdl ]]; then
+    a+=" "${changed_file}
+  fi
+done
+echo $a
