02636227909f85ae88bdee38e49c78d58facee0f
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2020-11-27 17:24:19 +0530

    Update hello.bash

--- .github/workflows/hello.bash
+++ .github/workflows/hello.bash
@@ -1,8 +1,2 @@
- 
- for changed_file in $1; do
-	 if [[ ${changed_file} = **.mdl ]]; then
-              a+=" "${changed_file}
-     fi
-  done
- 
- echo $a
+echo off
+echo Hello World from $AGENT_NAME.
